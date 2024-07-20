177f00b5cfebe7895b81e334daf6e3a7ae852348
Léonard POTHERAT <l.potherat@planetb.fr>
2024-07-20 07:36:35 +0200

    Initial commit

---
+++ src/Entity/Test2.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\Test2Repository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UlidType;
+use Symfony\Component\Uid\Ulid;
+
+#[ORM\Entity(repositoryClass: Test2Repository::class)]
+class Test2
+{
+    #[ORM\Id]
+    #[ORM\Column(type: UlidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.ulid_generator')]
+    private ?Ulid $id = null;
+
+    public function getId(): ?Ulid
+    {
+        return $this->id;
+    }
+}
